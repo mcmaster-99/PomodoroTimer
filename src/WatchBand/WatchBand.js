dbdaf92495535d9a35111ded97ff4f4fcc1cf2e2
Bilal <mcmaster-99>
2019-08-03 08:26:14 -0500

    Rendered state of timer with countdown start, pause, and reset functionalities

--- src/WatchBand/WatchBand.js
+++ src/WatchBand/WatchBand.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import './WatchBand.css';
+import Timer from '../Timer/Timer';
 
 class WatchBand extends Component {
 	render() {
 		return (
 			<div className="container">
-				<div className="WatchBand">
+				<div className="WatchBandLeftHalf">
+				</div>
+				<div className="WatchBandWhole">
+				</div>
+				<div className="WatchBandRightHalf">
+				</div>
+				<div className="WatchCase">
+					<Timer/>
 				</div>
 			</div>
 		);
