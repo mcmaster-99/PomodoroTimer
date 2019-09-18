819337f99fe7df296686d5b0a20906275381ea88
Bilal <mcmaster-99>
2019-09-18 17:22:18 -0500

    added status text and color highlighting for timer. If in session, timer color is white. If on break, timer color is green.

--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -7,9 +7,11 @@ class Timer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			minutes: 25,
-			seconds: '00',
-			isOn: false
+			minutes: 0,
+			seconds: '02',
+			isOn: false,
+			timeColor: "white",
+			status: "in session"
 		}
 	}
 	componentDidMount = () => {
@@ -19,12 +21,16 @@ class Timer extends Component {
 	startTimer = () => {
 
 		this.seconds = setInterval(() => {
-			if (this.state.seconds === 0 && this.state.minutes === 0) {
+			if (this.state.seconds === '00' && this.state.minutes === 0) {
+				this.setState({
+					timeColor: 'green',
+					status: 'on break'
+				})
 				clearInterval(this.seconds);	
 				alert("timer ended")
 			}
 			// if seconds is 0, reset it to 60 and decrement minutes
-			else if (this.state.seconds === '00'){
+			else if (this.state.seconds === '00' && this.state.minutes !== 0){
 				this.setState({minutes: this.state.minutes-1, seconds: 59})
 
 			// add 0 in front of single digit seconds
@@ -71,14 +77,16 @@ class Timer extends Component {
 
 	render() {
 
-		//let timeLeft = this.state.timeLeft;
+		console.log(typeof(this.state.timeColor))
 		
 		return (
 			<div className="TimerContainer">
 
 				<SessionTime increment={this.increment} decrement={this.decrement}/>
 
-				<p className="TimerText">{this.state.minutes}:{this.state.seconds}</p>
+				<div id="StatusText">You are now {this.state.status}</div>
+
+				<p className="TimerText" style={{color:this.state.timeColor}}>{this.state.minutes}:{this.state.seconds}</p>
 				<div className="ButtonContainer">
 
 					<button className="PlayButton" onClick={this.startTimer}>
